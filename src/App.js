a4b3eacf0402d529410436786c8f164a2c54f9be
Omry Zobel <omry@strigo.io>
2022-09-15 15:03:47 +0300

    Create drug search component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import DrugSearch from "./drug-search/drug-search";
 
-function App() {
+import "./app.scss";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div class="app-title">Drug Prescription App</div>
+      <div className="app-content">
+        <DrugSearch></DrugSearch>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/drug-search/drug-search.js
@@ -0,0 +1,57 @@
+import { AutoComplete, Input, Button } from "antd";
+import React, { useState } from "react";
+import { PlusOutlined } from "@ant-design/icons";
+
+import "./drug-search.scss";
+
+const DRUG_NAMES_INDEX = 1;
+const DRUG_SEARCH_API_URL =
+  "https://clinicaltables.nlm.nih.gov/api/rxterms/v3/search?terms=";
+
+export default function DrugSearch() {
+  const [options, setOptions] = useState([]);
+  const [value, setValue] = useState("");
+
+  const onSearch = async (searchText) => {
+    const rawDrugList = await fetch(`${DRUG_SEARCH_API_URL}${searchText}`);
+    const drugList = await rawDrugList.json();
+    const drugSearchOptions = drugList[DRUG_NAMES_INDEX].map((drug) => ({
+      value: drug,
+    }));
+
+    setOptions(!searchText ? [] : drugSearchOptions);
+  };
+
+  return (
+    <div className="drug-search">
+      <div class="search-wrapper">
+        <AutoComplete
+          options={options}
+          onSelect={setValue}
+          onSearch={onSearch}
+          style={{ width: "100%" }}
+          placeholder="Search for a drug"
+        >
+          <Input
+            style={{
+              width: "100%",
+              borderRadius: 4,
+              border: "1px solid #ccc",
+            }}
+          />
+        </AutoComplete>
+      </div>
+      <Button
+        type="primary"
+        style={{
+          borderRadius: 4,
+          height: "100%",
+          marginTop: 2,
+        }}
+        icon={<PlusOutlined />}
+      >
+        Add Drug
+      </Button>
+    </div>
+  );
+}
